c611b35d3c607c3ac74e654716593688e4f1fd6f
min <dr94406@naver.com>
2024-01-22 02:39:24 +0900

    chore: 커피도감 애플리케이션 추가

---
+++ src/main/java/com/easycoffee/backend/EasycoffeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.easycoffee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycoffeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycoffeeBackendApplication.class, args);
+	}
+
+}
