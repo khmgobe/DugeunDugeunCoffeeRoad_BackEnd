b22361319c13d09de94620a526a2728058609e31
min <dr94406@naver.com>
2024-01-23 22:07:52 +0900

    feat: 커피빈 엔티티 추가

---
+++ src/main/java/com/easycoffee/backend/coffeebean/entity/CoffeeBean.java
@@ -0,0 +1,37 @@
+package com.easycoffee.backend.coffeebean.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "COFFEE_BEAN")
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+public class CoffeeBean {
+
+    @Id @GeneratedValue
+    @Column(name = "bean_id")
+    private Long id;
+
+    @Column(name = "bean_name")
+    private String name;
+
+    @Column(name = "bean_country")
+    private String country;
+
+    @Column(name = "bean_variety")
+    private String variety;
+
+    @Column(name = "bean_note")
+    private String note;
+
+    @Column(name = "bean_roasting_level")
+    private String distribution;
+
+}
