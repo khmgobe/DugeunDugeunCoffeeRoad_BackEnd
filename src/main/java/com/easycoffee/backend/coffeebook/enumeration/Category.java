4be7cdb5827eb334525717ba84e42bbc8788c479
min <dr94406@naver.com>
2024-01-25 23:40:59 +0900

    feat: 커피도감 식별타입 enum 추가

---
+++ src/main/java/com/easycoffee/backend/coffeebook/enumeration/Category.java
@@ -0,0 +1,8 @@
+package com.easycoffee.backend.coffeebook.enumeration;
+
+import lombok.Getter;
+
+@Getter
+public enum Category {
+    COFFEEBABY_COLLECTION, BREWERS_COLLECTION, BASIC
+}
---
+++ src/main/java/com/easycoffee/backend/coffeebook/enumeration/Guideclassfication.java
@@ -0,0 +1,13 @@
+package com.easycoffee.backend.coffeebook.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Guideclassfication {
+
+    COFFEEBABY("커린이"), HOMEBARISTA("홈바리스타");
+
+    String description;
+}
