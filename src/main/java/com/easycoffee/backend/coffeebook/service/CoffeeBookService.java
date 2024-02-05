2c9ffd476a175270d5ba0484928d7da78fcf6758
min <dr94406@naver.com>
2024-02-05 22:59:46 +0900

    fix: Service 수정

--- src/main/java/com/easycoffee/backend/coffeebook/service/CoffeeBookService.java
+++ src/main/java/com/easycoffee/backend/coffeebook/service/CoffeeBookService.java
@@ -1,24 +1,21 @@
 package com.easycoffee.backend.coffeebook.service;
 
-import com.easycoffee.backend.coffeebook.dto.CoffeeBookResponseDto;
-import com.easycoffee.backend.coffeebook.dto.CoffeeBookSaveDto;
-import com.easycoffee.backend.coffeebook.dto.CoffeeBookUpdateDto;
+import com.easycoffee.backend.coffeebook.dto.response.CoffeeBookDeleteResponseDto;
+import com.easycoffee.backend.coffeebook.dto.response.CoffeeBookResponseDto;
+import com.easycoffee.backend.coffeebook.dto.request.CoffeeBookSaveDto;
+import com.easycoffee.backend.coffeebook.dto.request.CoffeeBookUpdateDto;
+import com.easycoffee.backend.coffeebook.dto.response.CoffeeBookSaveResponseDto;
+import com.easycoffee.backend.coffeebook.dto.response.CoffeeBookUpdateResponseDto;
 import com.easycoffee.backend.coffeebook.entity.CoffeeBook;
 import com.easycoffee.backend.coffeebook.repository.CoffeeBookRepository;
-import com.easycoffee.backend.coffeebook.enumeration.DeleteResponse;
 import com.easycoffee.backend.common.exception.BadRequestException;
 import com.easycoffee.backend.common.enumeration.CommonErrorType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
-@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -32,10 +29,10 @@ public class CoffeeBookService {
      * @param coffeeBookSaveDto 커피 도감 저장 Dto 객체입니다.
      * @return 커피 도감을 저장한 정보를 반환합니다.
      */
-    public CoffeeBookResponseDto save(CoffeeBookSaveDto coffeeBookSaveDto) {
+    public CoffeeBookSaveResponseDto save(CoffeeBookSaveDto coffeeBookSaveDto) {
         CoffeeBook coffeeBook = coffeeBookSaveDto.toEntity();
         coffeeBookRepository.save(coffeeBook);
-        return CoffeeBookResponseDto.response(coffeeBook);
+        return CoffeeBookSaveResponseDto.response(coffeeBook);
     }
 
     /**
@@ -56,28 +53,28 @@ public class CoffeeBookService {
         List<CoffeeBook> coffeeBookList = coffeeBookRepository.find();
         return coffeeBookList.stream().map(CoffeeBookResponseDto::response).toList();
     }
+
     /**
      * 커피 도감을 수정합니다.
      * @param coffeeBookUpdateDto 커피 도감 수정 Dto 객체입니다.
      * @return
      */
-    public CoffeeBookResponseDto update(CoffeeBookUpdateDto coffeeBookUpdateDto) {
+    public CoffeeBookUpdateResponseDto update(CoffeeBookUpdateDto coffeeBookUpdateDto) {
         CoffeeBook coffeeBook = coffeeBookRepository.findById(coffeeBookUpdateDto.getId());
         coffeeBook.updateBook(coffeeBookUpdateDto);
         coffeeBookRepository.update(coffeeBook);
-        return CoffeeBookResponseDto.response(coffeeBook);
+        return CoffeeBookUpdateResponseDto.response(coffeeBook);
     }
 
 
     /**
      * 커피 도감을 삭제합니다.
-     * 실패 케이스 구현 필요
-     * @param coffeeBookId 커피 도감 아이디입니다.
+     * @param id 커피 도감 아이디입니다.
      * @return 삭제 시 HttpStatus 응답코드 200번과 "SUCCESS"를 반환합니다.
      */
-    public ResponseEntity<DeleteResponse> remove(Long coffeeBookId) {
-        Optional.ofNullable(coffeeBookId).orElseThrow(() -> new BadRequestException(CommonErrorType.NO_ELEMENT));
-        coffeeBookRepository.remove(coffeeBookId);
-        return ResponseEntity.status(HttpStatus.OK).body(DeleteResponse.SUCCESS);
+    public CoffeeBookDeleteResponseDto remove(Long id) {
+        Optional.ofNullable(id).orElseThrow(() -> new BadRequestException(CommonErrorType.NO_ELEMENT));
+        coffeeBookRepository.remove(id);
+        return CoffeeBookDeleteResponseDto.deleteResponse(id);
     }
 }
