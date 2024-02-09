828df506737df9e6876d0cd2e43558e9af152da8
min <dr94406@naver.com>
2024-02-09 20:58:08 +0900

    fix: 스키마 디스크립션 추가, Dto 변환 코드 추가

--- src/main/java/com/easycoffee/backend/coffeebean/entity/CoffeeBean.java
+++ src/main/java/com/easycoffee/backend/coffeebean/entity/CoffeeBean.java
@@ -1,5 +1,9 @@
 package com.easycoffee.backend.coffeebean.entity;
 
+import com.easycoffee.backend.coffee.dto.CoffeeResponseDto;
+import com.easycoffee.backend.coffeebean.dto.CoffeeBeanResponseDto;
+import com.easycoffee.backend.coffeebean.dto.CoffeeBeanUpdateDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 import static lombok.AccessLevel.*;
@@ -14,21 +18,33 @@ public class CoffeeBean {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bean_id")
+    @Schema(description = "원두 아이디", hidden = true)
     private Long id;
 
     @Column(name = "bean_name")
+    @Schema(description = "원두 이름")
     private String name;
 
-    @Column(name = "bean_country")
-    private String country;
-
-    @Column(name = "bean_variety")
-    private String variety;
-
     @Column(name = "bean_note")
+    @Schema(description = "원두 노트")
     private String note;
 
-    @Column(name = "bean_roasting_level")
+    @Column(name = "bean_distribution")
+    @Schema(description = "원두 배전도")
     private String distribution;
 
+
+    public void updateCoffeeBean(CoffeeBeanUpdateDto coffeeBeanUpdateDto) {
+        this.name = coffeeBeanUpdateDto.getName();
+        this.note = coffeeBeanUpdateDto.getNote();
+        this.distribution = coffeeBeanUpdateDto.getDistribution();
+    }
+
+    public CoffeeBeanResponseDto toDto() {
+        return CoffeeBeanResponseDto.builder().
+                name(name).
+                note(note).
+                distribution(distribution).
+                build();
+    }
 }
