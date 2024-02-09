9460163ba47f2f2adb7d5146240b50f1ada964f4
min <dr94406@naver.com>
2024-02-09 14:48:27 +0900

    fix: 시간 반환타입 추가

--- src/main/java/com/easycoffee/backend/common/util/CommonControllerAdvice.java
+++ src/main/java/com/easycoffee/backend/common/util/CommonControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.easycoffee.backend.common.util;
 
-import com.easycoffee.backend.common.enumeration.CommonErrorType;
+import com.easycoffee.backend.common.enumeration.CommonResponseType;
 import com.easycoffee.backend.common.exception.BadRequestException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -12,39 +12,41 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class CommonControllerAdvice {
-        private final ErrorAttributes errorAttributes;
+    private final ErrorAttributes errorAttributes;
 
-        /** 잘못된 요청 예외 반환 **/
-        @ExceptionHandler(BadRequestException.class)
-        public ResponseEntity<?> BadRequestException(BadRequestException badRequestException, HttpServletRequest request) {
-            return CommonErrorResponse(badRequestException.getCommonErrorType(), request);
-        }
-
-
-        private ResponseEntity<?> CommonErrorResponse(CommonErrorType commonErrorType, HttpServletRequest request) {
-            Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions(request));
-            responseStatus(errorAttributes, commonErrorType);
-
-            return ResponseEntity.status(commonErrorType.getStatus()).body(errorAttributes);
-        }
-
-        private Map<String, Object> getErrorAttributes(HttpServletRequest request, ErrorAttributeOptions options) {
-            WebRequest webRequest = new ServletWebRequest(request);
-            return errorAttributes.getErrorAttributes(webRequest, options);
-        }
-
-        private ErrorAttributeOptions ErrorAttributeOptions(HttpServletRequest request) {
-            return ErrorAttributeOptions.defaults();
-        }
-
-        private void responseStatus(Map<String, Object> errorAttributes, CommonErrorType commonErrorType) {
-            errorAttributes.put("status", commonErrorType.getStatus());
-            errorAttributes.put("error", commonErrorType.getDescription());
-            errorAttributes.put("type", commonErrorType);
-        }
+    /** 잘못된 요청 예외 반환 **/
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> BadRequestException(BadRequestException badRequestException, HttpServletRequest request) {
+        return CommonErrorResponse(badRequestException.getCommonResponseType(), request);
     }
+
+
+    private ResponseEntity<?> CommonErrorResponse(CommonResponseType commonResponseType, HttpServletRequest request) {
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions(request));
+        responseStatus(errorAttributes, commonResponseType);
+
+        return ResponseEntity.status(commonResponseType.getHttpStatus()).body(errorAttributes);
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, ErrorAttributeOptions options) {
+        WebRequest webRequest = new ServletWebRequest(request);
+        return errorAttributes.getErrorAttributes(webRequest, options);
+    }
+
+    private ErrorAttributeOptions ErrorAttributeOptions(HttpServletRequest request) {
+        return ErrorAttributeOptions.defaults();
+    }
+
+    private void responseStatus(Map<String, Object> errorAttributes, CommonResponseType commonResponseType) {
+        errorAttributes.put("timestamp", LocalDateTime.now());
+        errorAttributes.put("status", commonResponseType.getHttpStatus());
+        errorAttributes.put("error", commonResponseType.getDescription());
+        errorAttributes.put("type", commonResponseType);
+    }
+}
